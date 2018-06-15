475979a3948571350f430e4aa8b2e4e34deba67b
kujirahand <web@kujirahand.com>
2018-06-15 16:43:10 +0900

    Update test_spam_ok.py
    消えた番号マークを追加

--- ch4/spam_checker/test_spam_ok.py
+++ ch4/spam_checker/test_spam_ok.py
@@ -24,13 +24,13 @@ tagger = MeCab.Tagger()
 # 学習済みモデルを読み出す --- (※3)
 model = pickle.load(open(model_file, "rb"))
 
-# テキストがスパムかどうか判定する
+# テキストがスパムかどうか判定する ---(*4)
 def check_spam(text):
     # テキストを単語IDのリストに変換し単語の頻出頻度を調べる
     zw = np.zeros(word_dic['__id'])
     count = 0
     s = tagger.parse(text)
-    # 単語毎の回数を加算
+    # 単語毎の回数を加算 --- (※5)
     for line in s.split("\n"):
         if line == "EOS": break
         params = line.split("\t")[1].split(",")
@@ -39,11 +39,11 @@ def check_spam(text):
             id = word_dic[org]
             zw[id] += 1
             count += 1
-    zw = zw / count
-    # 予測
+    zw = zw / count # --- (※6)
+    # 予測 --- (※7)
     pre = model.predict([zw])[0]
     print("- 結果=", label_names[pre])
 
-if __name__ == "__main__":
+if __name__ == "__main__": # --- (※8)
     check_spam(test_text1)
     check_spam(test_text2)
