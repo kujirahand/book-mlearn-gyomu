149b01a897eecd0b266bdaca23da49cf817ec3fb
kujirahand <kujira@kujirahand.com>
2020-06-22 14:11:59 +0900

    2章 ワインと天気のプログラムを微調整(紙面と一致するように)

--- src2/ch2/tenki/heikin.py
+++ src2/ch2/tenki/heikin.py
@@ -7,13 +7,13 @@ df = pd.read_csv("kion10y.csv", encoding="utf-8")
 md = {}
 for i, row in df.iterrows():
     m,  d, v = (int(row['月']), int(row['日']), float(row['気温']))
-    key = str(m) + "/" + str(d)
+    key = "{:02d}/{:02d}".format(m,d)
     if not(key in md): md[key] = []
     md[key] += [v]
 
 # 日付ごとに平均を求める ---(*3)
 avs = {}
-for key in md:
+for key in sorted(md):
     v = avs[key] = sum(md[key]) / len(md[key]) # ---(*4)
     print("{0} : {1}".format(key, v))
 
