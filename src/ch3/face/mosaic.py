9690e3758ba2685e2012b3c4974dda5344cdf494
kujirahand <kujira@kujirahand.com>
2020-09-28 20:30:45 +0900

    解説を補足

--- src/ch3/face/mosaic.py
+++ src/ch3/face/mosaic.py
@@ -6,7 +6,7 @@ def mosaic(img, rect, size):
     w = x2 - x1
     h = y2 - y1
     i_rect = img[y1:y2, x1:x2]
-    # 一度縮小して拡大する
+    # モザイク処理のため一度縮小して拡大する
     i_small = cv2.resize(i_rect, ( size, size))
     i_mos = cv2.resize(i_small, (w, h), interpolation=cv2.INTER_AREA)
     # 画像にモザイク画像を重ねる
